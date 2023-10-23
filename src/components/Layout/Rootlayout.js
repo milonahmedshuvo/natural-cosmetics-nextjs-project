7043c02f85e1272d2648be7b249bec6764c7c015
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-10-23 23:46:50 +0600

    project initialize

---
+++ src/components/Layout/Rootlayout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navber from '../Sheare/Navber/Navber'
+
+const Rootlayout = () => {
+  return (
+    <div>
+        <Navber></Navber>
+      
+    </div>
+  )
+}
+
+export default Rootlayout
